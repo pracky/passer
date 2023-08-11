51491e968564c588ac02b35bc0c571b111b999ae
pracky <42076147+pracky@users.noreply.github.com>
2023-08-11 13:08:41 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,57 @@
+# Link-Bypasser-Bot
+
+a Telegram Bot that can Bypass Ad Links and Generate Direct Links. see the Bot in Action [@BypassLinkBot](https://t.me/BypassLinkBot) or try it on [Replit](https://replit.com/@bipinkrish/Link-Bypasser#app.py)
+
+---
+
+## Required Variables
+
+- `TOKEN` Bot Token from @BotFather
+- `HASH` API Hash from my.telegram.org
+- `ID` API ID from my.telegram.org
+
+## Optional Variables 
+you can also set these in `config.json` file
+
+- `CRYPT` GDTot Crypt If you don't know how to get Crypt then [Learn Here](https://www.youtube.com/watch?v=EfZ29CotRSU)
+- `XSRF_TOKEN` and `Laravel_Session` XSRF Token and Laravel Session cookies! If you don't know how to get then then watch [this Video](https://www.youtube.com/watch?v=EfZ29CotRSU) (for GDTOT) and do the same for sharer.pw
+- `DRIVEFIRE_CRYPT` Drivefire Crypt
+- `KOLOP_CRYPT`  Kolop Crypt!
+- `HUBDRIVE_CRYPT` Hubdrive Crypt
+- `KATDRIVE_CRYPT` Katdrive Crypt
+- `UPTOBOX_TOKEN` Uptobox Token
+- `TERA_COOKIE` Terabox Cookie (only `ndus` value) (see [Help](#help))
+- `CLOUDFLARE` Use `cf_clearance` cookie from and Cloudflare protected sites
+
+---
+
+## Commands
+
+```
+start - Welcome Message
+help - List of All Supported Sites
+```
+
+---
+
+## Supported Sites
+
+To see the list of supported sites see [texts.py](https://github.com/bipinkrish/Link-Bypasser-Bot/blob/main/texts.py) file
+
+---
+
+## Help
+
+* If you are deploying on VPS, watch videos on how to set/export Environment Variables. OR you can set these in `config.json` file
+
+* Terabox Cookie
+
+    1. Open any Browser
+    2. Make sure you are logged in with a Terbox account
+    3. Press `f12` to open DEV tools and click Network tab
+    4. Open any Terbox video link and open Cookies tab
+    5. Copy value of `ndus`
+   
+   <br>
+
+   ![](https://i.ibb.co/hHBZM5m/Screenshot-113.png)
---
+++ main.py
@@ -0,0 +1,150 @@
+import pyrogram
+from pyrogram import Client,filters
+from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton
+from os import environ, remove
+from threading import Thread
+from json import load
+from re import search
+
+from texts import HELP_TEXT
+import bypasser
+from ddl import ddllist, direct_link_generator
+from time import time
+
+
+# bot
+with open('config.json', 'r') as f: DATA = load(f)
+def getenv(var): return environ.get(var) or DATA.get(var, None)
+
+bot_token = getenv("TOKEN")
+api_hash = getenv("HASH") 
+api_id = getenv("ID")
+app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token)  
+
+
+# handle ineex
+def handleIndex(ele,message,msg):
+    result = bypasser.scrapeIndex(ele)
+    try: app.delete_messages(message.chat.id, msg.id)
+    except: pass
+    for page in result: app.send_message(message.chat.id, page, reply_to_message_id=message.id, disable_web_page_preview=True)
+
+
+# loop thread
+def loopthread(message,otherss=False):
+
+    urls = []
+    if otherss: texts = message.caption
+    else: texts = message.text
+
+    if texts in [None,""]: return
+    for ele in texts.split():
+        if "http://" in ele or "https://" in ele:
+            urls.append(ele)
+    if len(urls) == 0: return
+
+    if bypasser.ispresent(ddllist,urls[0]):
+        msg = app.send_message(message.chat.id, "⚡ __generating...__", reply_to_message_id=message.id)
+    else:
+        if "https://olamovies" in urls[0] or "https://psa.wf/" in urls[0]:
+            msg = app.send_message(message.chat.id, "🔎 __this might take some time...__", reply_to_message_id=message.id)
+        else:
+            msg = app.send_message(message.chat.id, "🔎 __bypassing...__", reply_to_message_id=message.id)
+
+    strt = time()
+    links = ""
+    for ele in urls:
+        if search(r"https?:\/\/(?:[\w.-]+)?\.\w+\/\d+:", ele):
+            handleIndex(ele,message,msg)
+            return
+        elif bypasser.ispresent(ddllist,ele):
+            try: temp = direct_link_generator(ele)
+            except Exception as e: temp = "**Error**: " + str(e)
+        else:    
+            try: temp = bypasser.shortners(ele)
+            except Exception as e: temp = "**Error**: " + str(e)
+        print("bypassed:",temp)
+        if temp != None: links = links + temp + "\n"
+    end = time()
+    print("Took " + "{:.2f}".format(end-strt) + "sec")
+
+    if otherss:
+        try:
+            app.send_photo(message.chat.id, message.photo.file_id, f'__{links}__', reply_to_message_id=message.id)
+            app.delete_messages(message.chat.id,[msg.id])
+            return
+        except: pass
+
+    try: 
+        final = []
+        tmp = ""
+        for ele in links.split("\n"):
+            tmp += ele + "\n"
+            if len(tmp) > 4000:
+                final.append(tmp)
+                tmp = ""
+        final.append(tmp)
+        app.delete_messages(message.chat.id, msg.id)
+        tmsgid = message.id
+        for ele in final:
+            tmsg = app.send_message(message.chat.id, f'__{ele}__',reply_to_message_id=tmsgid, disable_web_page_preview=True)
+            tmsgid = tmsg.id
+    except Exception as e:
+        print(e)
+        try: app.send_message(message.chat.id, "__Failed to Bypass__", reply_to_message_id=message.id)
+        except:
+            app.send_message(message.chat.id, "__Failed to Bypass__", reply_to_message_id=message.id)
+
+
+# start command
+@app.on_message(filters.command(["start"]))
+def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
+    app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, I am Link Bypasser Bot hosted by 𝙏𝙚𝙖𝙢 𝙎𝙋𝙔, just send me any supported links and i will you get you results.\nCheckout /help to Read More__",
+    reply_markup=InlineKeyboardMarkup([
+        [ InlineKeyboardButton("🌐 𝙅𝙤𝙞𝙣 𝙎𝙪𝙥𝙥𝙤𝙧𝙩 𝘾𝙝𝙖𝙣𝙣𝙚𝙡", url="https://t.me/spy_adda_official")],
+        [ InlineKeyboardButton("𝙊𝙬𝙣𝙚𝙧", url="https://t.me/gazabho") ]]), 
+        reply_to_message_id=message.id)
+
+
+# help command
+@app.on_message(filters.command(["help"]))
+def send_help(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
+    app.send_message(message.chat.id, HELP_TEXT, reply_to_message_id=message.id, disable_web_page_preview=True)
+
+
+# links
+@app.on_message(filters.text)
+def receive(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
+    bypass = Thread(target=lambda:loopthread(message),daemon=True)
+    bypass.start()
+
+
+# doc thread
+def docthread(message):
+    msg = app.send_message(message.chat.id, "🔎 __bypassing...__", reply_to_message_id=message.id)
+    print("sent DLC file")
+    file = app.download_media(message)
+    dlccont = open(file,"r").read()
+    links = bypasser.getlinks(dlccont)
+    app.edit_message_text(message.chat.id, msg.id, f'__{links}__', disable_web_page_preview=True)
+    remove(file)
+
+
+# files
+@app.on_message([filters.document,filters.photo,filters.video])
+def docfile(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
+    
+    try:
+        if message.document.file_name.endswith("dlc"):
+            bypass = Thread(target=lambda:docthread(message),daemon=True)
+            bypass.start()
+            return
+    except: pass
+
+    bypass = Thread(target=lambda:loopthread(message,True),daemon=True)
+    bypass.start()
+
+
+# server loop
+print("Bot Starting")
+app.run()
